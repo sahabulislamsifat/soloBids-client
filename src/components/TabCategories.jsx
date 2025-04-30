817461a6bb7fa8a74095b36aa4152c19780e00a8
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-04-30 12:59:31 +0600

    implement tanstackquery for data fetching

--- src/components/TabCategories.jsx
+++ src/components/TabCategories.jsx
@@ -2,20 +2,20 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import JobCard from "./JobCard";
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./LoadingSpinner";
 
 const TabCategories = () => {
-  const [jobs, setJobs] = useState([]);
+  const { data: jobs, isLoading } = useQuery({
+    queryKey: ["jobs"],
+    queryFn: async () => {
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/jobs`);
+      return data;
+    },
+  });
 
-  useEffect(() => {
-    fetchAllJobs();
-  }, []);
-
-  const fetchAllJobs = async () => {
-    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/jobs`);
-    setJobs(data);
-  };
+  if (isLoading) return <LoadingSpinner></LoadingSpinner>;
 
   return (
     <Tabs>
